98931f0f580abb71a7250998b7818e2daab8404d
ananta580 <anantapoudel580@gmail.com>
2023-02-14 17:45:41 +0545

    Did changes for package export

--- projects/ngc-showcase/src/app/app.module.ts
+++ projects/ngc-showcase/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'projects/ngc-ui/src/lib/button/button.module';
+import { ButtonModule } from 'ngc-ui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
